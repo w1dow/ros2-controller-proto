ca40a5d42c075c5f89c32e08ff30ff00e2235b03
w1dow <avinashwrk1306@gmail.com>
2026-04-12 11:09:26 +0530

    Final_commit

--- ros2_ws1/src/control/controller/controller/controller.py
+++ ros2_ws1/src/control/controller/controller/controller.py
@@ -32,7 +32,6 @@ class Controller(Node):
             self.get_logger().info("Waiting  spawn service...")
         while not self.robo_kill.wait_for_service(timeout_sec=1.0):
             self.get_logger().info("Waiting  kill service...")
-
         self.spawn_turtle(0.5,0.5,"robo1")
 
         self.load_waypoints()
@@ -40,6 +39,7 @@ class Controller(Node):
         if self.waypoints:
             self.set_goal(0)
             if len(self.waypoints) > 1:
+                self.kill_turtle("turtle1")
                 wp = self.waypoints[0]
                 self.spawn_turtle(wp['x'], wp['y'], "wp_0")
 
@@ -62,11 +62,11 @@ class Controller(Node):
 
         future = self.robo_spawn.call_async(req)
 
-    def kill_turtle(self,id):
+    def kill_turtle(self,name):
         while not self.robo_kill.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('Waiting for spawn service...')
+            self.get_logger().info('Waiting for kill service...')
         req=Kill.Request()
-        req.name=f"wp_{id}"
+        req.name=name
         future=self.robo_kill.call_async(req)
 
 
@@ -91,12 +91,6 @@ class Controller(Node):
             self.waypoints = []
 
 
-    def check(self):
-        distance=math.sqrt(math.pow((self.x-self.goal_x),2)+math.pow((self.y-self.goal_y),2))
-        if distance <= self.thres:
-            self.get_logger().info(f"Reached target = {self.goal_x},{self.goal_y}")
-
-
     def look(self):
         target_angle = math.atan2(
             self.goal_y - self.y,
@@ -107,14 +101,14 @@ class Controller(Node):
             math.sin(target_angle - self.angle),
             math.cos(target_angle - self.angle)
         )
-        # self.get_logger().info(f"Target: {target_angle},Self_angle: {self.angle}, error: {e}")
+        self.get_logger().info(f"Target: {target_angle},Self_angle: {self.angle}, error: {e}")
         return e
 
     def set_goal(self,id):
         wp = self.waypoints[id]
         self.goal_x = wp['x']
         self.goal_y = wp['y']
-        self.get_logger().info(f"Target {id} :({self.goal_x},{self.goal_y})")
+        # self.get_logger().info(f"Target {id} :({self.goal_x},{self.goal_y})")
 
 
     def get_distance(self):
@@ -126,10 +120,10 @@ class Controller(Node):
             return 
 
         self.get_logger().info(f"Reached waypoint : id{self.target_id} @ ({self.goal_x},{self.goal_y})")
-        self.kill_turtle(self.target_id)
+        self.kill_turtle(f"wp_{self.target_id}")
         self.target_id+=1
         self.set_goal(self.target_id)
-        self.get_logger().info(f"id = {self.target_id}, {self.goal_x}, {self.goal_y}")
+        # self.get_logger().info(f"id = {self.target_id}, {self.goal_x}, {self.goal_y}")
         self.spawn_turtle(self.goal_x,self.goal_y,f"wp_{self.target_id}")
 
 
